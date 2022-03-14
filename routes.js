e67bf002940731ee04d97c5a11f4bba563dfa7d6
Mateeusgl <mateeusgl@pop-os.localdomain>
2022-03-13 23:23:00 -0300

    first commit

---
+++ routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const routes = express.Router();
+const instructors = require("./instructors");
+
+routes.get("/", (req, res) => {
+  return res.redirect("/instructors");
+});
+
+routes.get("/instructors", (req, res) => {
+  return res.render("instructors/index");
+});
+
+routes.get("/instructors/create", (req, res) => {
+  return res.render("instructors/create");
+});
+
+routes.get("/members", (req, res) => {
+  return res.send("members");
+});
+
+routes.post("/instructors", instructors.post);
+
+module.exports = routes;
