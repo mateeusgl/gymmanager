3ba935b3ef1879171b61c20fcc0e56abb087edcb
mateeusgl <magolopes20@gmail.com>
2022-03-20 02:43:03 -0300

    Create all part of date

--- instructors.js
+++ instructors.js
@@ -1,23 +1,83 @@
+const { age, date } = require("./utils");
+// Fs is lib of write date
 const fs = require("fs");
+// This is our DB while we not have one structured;
 const data = require("./data.json");
 
+exports.show = (req, res) => {
+  const { id } = req.params
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id == id
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found!!")
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+  }
+
+  return res.render("instructors/show", { instructor })
+}
+
 exports.post = (req, res) => {
+  // We are catching all the values of form;
   const keys = Object.keys(req.body);
 
+  // Using the expression FOR to verify if inputs is filled;
   for (key of keys) {
     if (req.body[key] == "") {
       return res.send("Please, fill all fields");
     }
   }
 
-  req.body.birth = Date.parse(req.body.birth);
-  req.body.created_at = Date.now();
+  // Destructureding datas of instructor
+  let {
+    avatar_url,
+    birth,
+    name,
+    services,
+    gender,
+  } = req.body
 
-  data.instructors.push(req.body);
+  // Structured datas for send to database;
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.instructors.length + 1)
 
+  // Send datas to database;
+  data.instructors.push({ 
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at
+  });
+
+  // Using FS for write dates in data.json while DB not is created;
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Write file error!");
 
     return res.redirect("/instructors");
   });
 };
+
+exports.edit = (req, res) => {
+  const { id } = req.params
+  const foundInstructor = data.instructors.find((instructor) => {
+    return instructor.id == id
+  })
+  
+  if (!foundInstructor) return res.send("Instructor not found!!")
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+
+  return res.render("instructors/edit", { instructor })
+}
--- routes.js
+++ routes.js
@@ -14,6 +14,10 @@ routes.get("/instructors/create", (req, res) => {
   return res.render("instructors/create");
 });
 
+routes.get("/instructors/:id", instructors.show);
+
+routes.get("/instructors/:id/edit", instructors.edit);
+
 routes.get("/members", (req, res) => {
   return res.send("members");
 });
